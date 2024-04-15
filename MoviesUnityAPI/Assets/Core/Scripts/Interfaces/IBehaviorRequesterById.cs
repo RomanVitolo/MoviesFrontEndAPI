0800252c04202dc22a338bd3e0323a5f6bb83ce7
Roman Vitolo <roman@7r1ck.com>
2024-04-15 11:08:43 -0300

    remove rpg asset

--- MoviesUnityAPI/Assets/Core/Scripts/Interfaces/IBehaviorRequesterById.cs
+++ MoviesUnityAPI/Assets/Core/Scripts/Interfaces/IBehaviorRequesterById.cs
@@ -6,6 +6,6 @@ namespace Interfaces
     public interface IBehaviorRequesterById
     {
         public Task CallRequestMethodById<T>(string apiController, string idType = null);
-        public event Action<object> OnGetResult;  
+        public event Action<object> OnGetResult;      
     }            
 }
